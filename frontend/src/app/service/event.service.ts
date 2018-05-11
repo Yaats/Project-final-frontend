56a0761b9713a4281a545af64e2308998055b78d
Yaats <trang.billy@live.fr>
2018-05-11 11:19:26 +0200

    dsq

--- frontend/src/app/service/event.service.ts
+++ frontend/src/app/service/event.service.ts
@@ -24,13 +24,4 @@ export class Event {
   url: string;
   title: string;
   realDateStart: string;
-  // vote_count: number;
-  // id: number;
-  // title: string;
-  // popularity: number;
-  // poster_path: string;
-  // release_date: string;
-  // ? means it's not required to have one
-  // createdAt?: Date;
-  // updatedAd?: Date;
 }
