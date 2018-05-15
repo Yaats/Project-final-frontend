09dbfe6ee5db95ac7ecffcf17d9cc05a23abb1ef
Elise Griset <elise.griset@gmail.com>
2018-05-15 14:48:55 +0200

    add list

--- frontend/src/app/favorite/favorite.component.ts
+++ frontend/src/app/favorite/favorite.component.ts
@@ -13,35 +13,38 @@ export class FavoriteComponent implements OnInit {
   list: Array<Movie | Event | Serie> = [];
   user: User;
   creds: SignupCredentials = new SignupCredentials();
-  _id: string;
+  userList: Array<string> = [];
 
   constructor(public dbList: ListService, private userServ: UserService) {}
 
   ngOnInit() {
-    this.userServ.check().then(() => {}).catch(err => {
-      console.log('app login error', err);
-    });
     this.dbList.getList().then((response: any) => {
-      console.log('voici la dblist', response);
       this.list = response;
+      this.userList = response;
+      console.log(this.list);
+      console.log(this.userList);
     });
   }
 
-  deleteClick() {
-    // const {  } = this.list;
+  // deleteClick() {
+  //   for (var l = 0; l < this.list.length; l++) {
+  //     var aList = this.list[l];
+  //   }
 
-    // const isOkay = confirm(`Are you sure you want to delete ${} from your list?`);
-    // if (!isOkay) {
-    //   return;
-    // }
-    this.dbList
-      .delete(this._id)
-      .then(result => {
-        console.log(result);
-      })
-      .catch(err => {
-        console.log('Fav delete error');
-        console.log(err);
-      });
-  }
+  //   const isOkay = confirm(
+  //     `Are you sure you want to delete ${aList.title} from your list?`
+  //   );
+  //   if (!isOkay) {
+  //     return;
+  //   }
+  //   this.dbList
+  //     .delete(this._id)
+  //     .then(result => {
+  //       console.log(result);
+  //     })
+  //     .catch(err => {
+  //       console.log('Fav delete error');
+  //       console.log(err);
+  //     });
+  // }
 }
